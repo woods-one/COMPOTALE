236a07b5813f86fa1d883a66f85f31a2db086828
woods-one <0820ricer@gmail.com>
2022-07-31 23:47:20 +0900

    修正:GetComponentをSerializeFieldに修正しました

--- Assets/Scripts/BgmStop.cs
+++ Assets/Scripts/BgmStop.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 /// <summary>
 /// ゲームクリア時にBGMストップさせるスクリプト
 /// </summary>
-
 public class BgmStop : MonoBehaviour
 {
+    [SerializeField]
     AudioSource mianBGM;
+    
     void Update()
     {
         if (Timer.countTime <= 0)
         {
-            mianBGM = this.GetComponent<AudioSource>();
             mianBGM.Stop();
         }
     }
